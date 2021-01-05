36cbbb7fa014c234e7253b1c38f03fa71438b12b
ciochetta <luisfelipeciochetta@gmail.com>
2021-01-04 23:53:01 -0300

    finished update

--- commandExecutor.js
+++ commandExecutor.js
@@ -3,7 +3,8 @@ const insert = require("./commands/insert");
 const create = require("./commands/create");
 const using = require("./commands/using");
 const deleteCommand = require("./commands/delete");
+const update = require("./commands/update");
 
-const commands = [select, insert, create, using, deleteCommand];
+const commands = [select, insert, create, using, deleteCommand, update];
 
 module.exports = { commands };
